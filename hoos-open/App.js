fd71b8b313c27a866b4d1281c827c1012393df8b
Saketh Vangara <sakethvangara@mac.mynetworksettings.com>
2025-08-02 16:05:00 -0400

    added animation to login button and transition from login to home screen

--- hoos-open/App.js
+++ hoos-open/App.js
@@ -1,20 +1,50 @@
 // App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator, TransitionPresets } from '@react-navigation/native-stack';
+import { Animated, Easing } from 'react-native';
 
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
-import DetailsScreen from './screens/DetailsScreen'; // <-- Make sure this file exists and is correctly implemented
+import DetailsScreen from './screens/DetailsScreen';
 
 const Stack = createNativeStackNavigator();
+
+// Fade transition configuration
+const fadeConfig = {
+  animation: 'fade',
+  config: {
+    duration: 400,
+    easing: Easing.out(Easing.poly(4)),
+  },
+};
+
+const forFade = ({ current }) => ({
+  cardStyle: {
+    opacity: current.progress,
+  },
+});
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            animation: 'fade',
+            cardStyleInterpolator: forFade,
+            transitionSpec: {
+              open: fadeConfig,
+              close: fadeConfig,
+            },
+          }}
+        />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
